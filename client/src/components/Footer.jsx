a45be523e03cc0898ba6a4b7ae88d1b9daccd6b3
kevin belin <sorrento974@gmail.com>
2024-04-11 22:18:20 +0200

    coding footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import "../styles/footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <img src="src/assets/images/1.png" alt="logo cocktail club" />
+      <ul>
+        <li>Summer</li>
+        <li>Winter</li>
+        <li>Autumn</li>
+        <li>Spring</li>
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
