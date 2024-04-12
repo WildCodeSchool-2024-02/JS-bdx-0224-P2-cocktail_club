818bc6b8cf24a37fc9f963b8dbd5e8f80ec33f88
JulienChevreauPro <julienchevreau33@gmail.com>
2024-04-12 09:57:58 +0200

    first commit?

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./Navbar.css";
+
+function Navbar () {
+
+    const [menuState, setMenuState] = useState ({
+        burgerClass:"≡",
+        menuClass:"hide-menu",
+        isMenuClicked:false
+    });
+
+    const updateMenu = () => {
+            setMenuState(previousState =>({
+                burgerClass: previousState.isMenuClicked? "≡":"x",
+                menuClass: previousState.isMenuClicked? "hide-menu":"unhide-menu",
+                isMenuClicked: !previousState.isMenuClicked
+            }));
+        };
+
+    return (
+        <nav>
+            <img src="src\assets\images\Logo_Cocktails_Club.png" alt="Logo Cocktail Club"/>
+            <h1>COCKTAIL CLUB</h1>
+            <button type="button" onClick={updateMenu} onKeyDown={updateMenu}>{menuState.burgerClass}</button>
+            <ul className={menuState.menuClass}>
+                <li>Surprise cocktail</li>
+                <li>All our club cocktails</li>
+                <li>Back home</li>
+            </ul>
+        </nav>
+    )
+} 
+
+export default Navbar;
