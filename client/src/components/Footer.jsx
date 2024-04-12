7f0e4df97fa9fc2dcedad157bf577f5942270902
kevin belin <sorrento974@gmail.com>
2024-04-12 10:02:04 +0200

    add ancre

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,12 +3,12 @@ import "../styles/footer.css";
 function Footer() {
   return (
     <footer>
-      <img src="src/assets/images/1.png" alt="logo cocktail club" />
+      <a href="#ancreToTheTop"><img src="src/assets/images/1.png" alt="logo cocktail club" /></a>
       <ul>
-        <li>Summer</li>
-        <li>Winter</li>
-        <li>Autumn</li>
-        <li>Spring</li>
+        <li><a href="linkk">Summer</a></li>
+        <li><a href="link">Winter</a></li>
+        <li><a href="link">Autumn</a></li>
+        <li><a href="link">Spring</a></li>
       </ul>
     </footer>
   );
