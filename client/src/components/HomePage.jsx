9501c58366a9e9cdb9655303c188e150ae37bfa2
Franck <contact@franckwebpro.com>
2024-04-16 12:26:50 +0200

    fix before PR to dev

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -1,20 +1,14 @@
 import SeasonCard from "./SeasonCard";
-import AllCocktails from "./AllCocktails";
-import CocktailsSection from "./CocktailsSection";
-import Api from "./Api";
 
 function HomePage() {
   return (
-    <main>
+    <main className="homePage">
       <section className="season-section">
       <SeasonCard season="Spring" />
       <SeasonCard season="Summer" />
       <SeasonCard season="Autumn" />
       <SeasonCard season="Winter" />
       </section>
-      <CocktailsSection />
-      <AllCocktails />
-      <Api />
     </main>
   );
 }
