ca843724e673f05dcfeed7419a41d35b8baa0980
Franck <contact@franckwebpro.com>
2024-04-19 11:20:23 +0200

    API call and display by loader

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,6 +12,37 @@ import CategoryPage from "./pages/CategoryPage";
 
 // router creation
 
+async function fetchCocktailsBySeason(ingredient) {
+  const response = await fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
+  );
+  const data = await response.json();
+  return data.drinks.slice(0, 10);
+}
+
+const getCocktails = (id) => {
+
+let ingredient;
+    switch (id) {
+      case "summer":
+        ingredient = "Pineapple juice";
+        break;
+      case "autumn":
+        ingredient = "Blended whiskey";
+        break;
+      case "winter":
+        ingredient = "Kahlua";
+        break;
+      case "spring":
+        ingredient = "Sweet Vermouth";
+        break;
+      default:
+        ingredient = "";
+    }
+
+    return fetchCocktailsBySeason(ingredient);
+}
+
 const router = createBrowserRouter([
   {
     element: <App />,
@@ -23,7 +54,7 @@ const router = createBrowserRouter([
       {
         path: "/season/:id",
         element: <CategoryPage />,
-        // loader: ({ params }) => (params.id)
+        loader: ({ params }) => getCocktails(params.id),
       },
       {
         path: "/cocktails/:id",
--- client/src/pages/CategoryPage.jsx
+++ client/src/pages/CategoryPage.jsx
@@ -1,59 +1,12 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import "../styles/CategoryPage.css";
 import CocktailCard from "../components/CocktailCard";
 
 function CategoryPage() {
-  const { id } = useParams();
-  const [cocktails, setCocktails] = useState([]);
-  const [seasonTitle, setSeasonTitle] = useState("");
-
-  async function fetchCocktailsBySeason(ingredient) {
-    const response = await fetch(
-      `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`
-    );
-    const data = await response.json();
-    return data.drinks.slice(0, 10);
-  }
-
-  useEffect(() => {
-    let ingredient;
-    let title;
-    switch (id) {
-      case "summer":
-        ingredient = "Pineapple juice";
-        title = "Summer Cocktails";
-        break;
-      case "autumn":
-        ingredient = "Blended whiskey";
-        title = "Autumn Cocktails";
-        break;
-      case "winter":
-        ingredient = "Kahlua";
-        title = "Winter Cocktails";
-        break;
-      case "spring":
-        ingredient = "Sweet Vermouth";
-        title = "Spring Cocktails";
-        break;
-      default:
-        ingredient = "";
-        title = "";
-    }
-
-    setSeasonTitle(title);
-
-    const getCocktails = async () => {
-      const selectedCocktails = await fetchCocktailsBySeason(ingredient);
-      setCocktails(selectedCocktails);
-    };
-
-    getCocktails();
-  }, [id]);
+  const cocktails = useLoaderData();
 
   return (
     <main className="categoryPage">
-      {cocktails.length > 0 && <h2>{seasonTitle}</h2>}
       {cocktails.map((cocktail) => (
         <CocktailCard key={cocktail.idDrink} drink={cocktail} />
       ))}
